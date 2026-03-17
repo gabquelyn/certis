e2f07244acb00e951c0e681c69c3436211bee7b1
Arebamen <gabquelyn@gmail.com>
2026-03-17 14:42:56 -0700

    removed pre rendering for maps

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,23 +1,23 @@
+"use client";
 import React from "react";
 import dynamic from "next/dynamic";
+// import ContactMap from "./components/ContactMap";
 import Hero from "./components/Hero";
 import ContactForm from "./components/ContactForm";
-export const metadata = {
-  title: "Contact Us - Certis",
-  description:
-    "Get in touch with the Certis team for inquiries, support, or collaborations.",
-  keywords: ["Certis", "contact", "support", "inquiries", "team"],
-};
-
-const ContactMapNoSSR = dynamic(() => import("./components/ContactMap"), {
+// export const metadata = {
+//   title: "Contact Us - Certis",
+//   description:
+//     "Get in touch with the Certis team for inquiries, support, or collaborations.",
+//   keywords: ["Certis", "contact", "support", "inquiries", "team"],
+// };
+const ContactMap = dynamic(() => import("./components/ContactMap"), {
   ssr: false,
 });
-
 export default function page() {
   return (
     <section>
       <Hero />
-      <ContactMapNoSSR />
+      <ContactMap />
       <ContactForm />
     </section>
   );
